b129c96f516c76d4866059ee5b6186e3c55fc1fc
Mauro Titimoli <mgtitimoli@gmail.com>
2015-10-28 15:34:12 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# es-verify
+
+Verifies specified files to be compliant with given ECMAScript version
+
+### Installation
+
+`npm install --global es-verify`
+
+### Usage
+
+es-verify [OPTION]... [GLOB]...
+
+#### Options
+
+-w, --watch
+-e, --es-version
---
+++ bin/index.js
@@ -0,0 +1,35 @@
+var gulp   = require("gulp");
+var eslint = require("gulp-eslint");
+var path   = require("path");
+var argv   = require("minimist")(process.argv.slice(2), {
+    boolean: [ "watch" ],
+    alias  : {
+        watch: "-w"
+    }
+});
+
+// if (argv._.length === 0) {
+//     // ERROR
+// }
+
+gulp.task("lint", function () {
+
+    return gulp.src(argv._)
+        .pipe(eslint({
+            configFile : __dirname + "/../config/eslint.json",
+            reset      : true,
+            useEslintrc: false
+        }))
+        .pipe(eslint.format());
+});
+
+gulp.task("watch", [ "lint" ], function () {
+
+    gulp.watch(argv._, [ "lint" ]);
+});
+
+gulp.start(
+    argv.watch ?
+        "watch" :
+        "lint"
+);
